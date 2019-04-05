8102eac6fbf1088e628b15299f70d58c630bc79b
genuinebyte <bja@genuinebyte.com>
2019-04-05 08:49:15 -0500

    Switch to mio async sockets
    Still doesn't work though. read_tls returns non-zero but the same can't
    be said for read

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# upgraded-memory
+A collection of code snippets and tests on GitHub because I don't know what else to do with them.
+
+#### gendev_http
+My first time working with Rust TcpStream. Makes a GET request to [my website](genbyte.dev).
+
+#### gendev_https
+Trying to get find the minimum amount of code that will fetch [my website's homepage](genbyte.dev).
+
+The first shot at this (5ad1e691f3600c921089c30ac172652ef53f7da3) was using std::net::TcpStream
+sockets. These did not work. I went looking on the #rust IRC for help and we came to the conclusion
+that rustls wasn't designed for blocking IO.
--- gendev_https/src/main.rs
+++ gendev_https/src/main.rs
@@ -1,66 +1,89 @@
+use mio::event::Events;
+use mio::net::TcpStream;
+use mio::{Poll, PollOpt, Ready, Token};
 use rustls::{ClientConfig, ClientSession, Session};
 use std::io::{Read, Write};
-use std::net::{TcpStream, ToSocketAddrs};
+use std::net::ToSocketAddrs;
 use std::sync::Arc;
-use std::thread;
-use std::time::Duration;
 use webpki::DNSNameRef;
 
 fn main() {
-	let genuine_addrs = "genbyte.dev:443"
-		.to_socket_addrs()
-		.expect("Failed to parse hostname");
+	let CLIENT = Token(0);
+	let mut tcps = get_connection();
+
+	let poll = Poll::new().unwrap();
+	poll.register(
+		&tcps,
+		CLIENT,
+		Ready::readable() | Ready::writable(),
+		PollOpt::edge(),
+	)
+	.unwrap();
 
 	let mut config = ClientConfig::new();
 	config
 		.root_store
 		.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
-
 	let arc_config = Arc::new(config);
-	let genuine_com = DNSNameRef::try_from_ascii_str("genbyte.dev").unwrap();
-	let mut tls = ClientSession::new(&arc_config, genuine_com);
+	let genbyte_dev = DNSNameRef::try_from_ascii_str("genbyte.dev").unwrap();
+	let mut client = ClientSession::new(&arc_config, genbyte_dev);
+
+	client.write(b"GET / HTTP/1.0\r\n\r\n").unwrap();
+
+	let mut events = Events::with_capacity(64);
+	let mut wrote_request = false;
+	loop {
+		poll.poll(&mut events, None).unwrap();
+
+		for event in events.iter() {
+			match event.token() {
+				CLIENT => {
+					if !wrote_request && event.readiness().is_writable() && client.wants_write() {
+						print!("Trying to write request...");
+						println!("{} bytes", client.write_tls(&mut tcps).unwrap());
+						wrote_request = true;
+					}
+
+					if event.readiness().is_readable() && client.wants_read() {
+						//read_tls returns non-zero
+						let tls_read_size = client.read_tls(&mut tcps).unwrap();
+						//No isse on process_new_packets
+						client.process_new_packets().unwrap();
+
+						let mut plain = Vec::new();
+						//read_to_end returns Ok(0)??
+						let plain_read_size = client.read_to_end(&mut plain).unwrap();
+
+						let ascii = String::from_utf8(plain).unwrap();
+						println!(
+							"Response[{}|{}]:\n{}",
+							tls_read_size, plain_read_size, ascii
+						);
+						return;
+					}
+				}
+			}
+		}
+	}
+}
+
+fn get_connection() -> TcpStream {
+	let genuine_addrs = "genbyte.dev:443"
+		.to_socket_addrs()
+		.expect("Failed to parse hostname");
 
 	let mut genuine_connection: Option<TcpStream> = None;
 	for addr in genuine_addrs {
-		if let Ok(val) = TcpStream::connect(addr) {
+		if let Ok(val) = TcpStream::connect(&addr) {
+			println!("Using address: {}", addr);
 			genuine_connection = Some(val);
 			break;
 		}
 	}
 
-	let mut tcps = if let Some(val) = genuine_connection {
+	if let Some(val) = genuine_connection {
 		val
 	} else {
 		panic!("Failed to connect to genbyte.dev");
-	};
-
-	// In a pitiful attempt to get this code to work (doesn't return a response) I tried to wait
-	// until the session wants to read/write. Still doesn't work...
-	let tenth = Duration::from_millis(100);
-	loop {
-		if tls.wants_write() {
-			tls.write(b"GET / HTTP/1.0\r\n\r\n").unwrap();
-			tls.write_tls(&mut tcps).unwrap();
-			break;
-		}
-
-		thread::sleep(tenth);
-	}
-
-	loop {
-		if tls.wants_read() {
-			tls.read_tls(&mut tcps).unwrap();
-			tls.process_new_packets().unwrap();
-
-			let mut plain = Vec::new();
-			tls.read_to_end(&mut plain).unwrap();
-
-			let ascii = String::from_utf8_lossy(&plain);
-			println!("Response:\n{}", ascii);
-
-			return;
-		}
-
-		thread::sleep(tenth);
 	}
 }
